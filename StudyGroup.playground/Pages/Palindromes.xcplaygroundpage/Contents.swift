f9510ee4fc0074b2ab879024457e195bb5383949
Ryan Grey <rgrey@scottlogic.co.uk>
2015-07-09 09:09:38 +0100

    Adds solution for Palidromes question as a Playground page.

--- StudyGroup.playground/Pages/Palindromes.xcplaygroundpage/Contents.swift
+++ StudyGroup.playground/Pages/Palindromes.xcplaygroundpage/Contents.swift
@@ -1,5 +1,46 @@
-//: Playground - noun: a place where people can play
+//: # Palindromes
 
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+func isPalindrome(string: String) -> Bool {
+    
+    let allowedChars = NSCharacterSet.alphanumericCharacterSet()
+    let ignoredChars = allowedChars.invertedSet
+    
+    let strippedStringComponents: [String] = string.componentsSeparatedByCharactersInSet(ignoredChars)
+    let strippedString: String = strippedStringComponents.reduce("", combine: {$0 + $1})
+    
+    let lowercaseString = strippedString.lowercaseString
+    let reversedString = String(lowercaseString.characters.reverse())
+    
+    return lowercaseString == reversedString
+}
+
+//: Expect `true`
+
+isPalindrome("Animal loots foliated detail of stool lamina.")
+isPalindrome("Anna")
+isPalindrome("Anne, I vote more cars race Rome to Vienna.")
+isPalindrome("Are Mac ‘n’ Oliver ever evil on camera?")
+isPalindrome("Are we not drawn onward to new era?")
+isPalindrome("Are we not drawn onward, we few, drawn onward to new era?")
+isPalindrome("Are we not pure? “No sir!” Panama’s moody Noriega brags. “It is garbage!” Irony dooms a man; a prisoner up to new era.")
+isPalindrome("Art, name no tub time. Emit but one mantra.")
+isPalindrome("As I pee, sir, I see Pisa!")
+isPalindrome("Avid diva.")
+
+//: Weird `true` cases
+isPalindrome("")
+isPalindrome("     ")
+isPalindrome("b")
+isPalindrome("bbb")
+isPalindrome(".")
+isPalindrome("...")
+isPalindrome("      !")
+isPalindrome("!     !     ")
+isPalindrome("  !  ?  ")
+
+//: Expect `false`
+isPalindrome("Not a palindome!")
+isPalindrome("Ryan")
+isPalindrome("123")
