63e177f2bd2bd3a24a77e6ce4c056dcf051a6df2
Ryan Grey <rgrey@scottlogic.co.uk>
2015-07-09 10:01:07 +0100

    Adds hasWellFormedBrackets implementation.

--- StudyGroup.playground/Pages/Brackets.xcplaygroundpage/Contents.swift
+++ StudyGroup.playground/Pages/Brackets.xcplaygroundpage/Contents.swift
@@ -11,8 +11,74 @@ import Foundation
 
 //: ## Implementation
 
+class Bracket {
+    
+    let character: Character
+    
+    init?(potentialBracket: Character) {
+        character = potentialBracket
+        if !isBracket(potentialBracket) {
+            return nil
+        }
+    }
+    
+    private func isBracket(potentialBracket: Character) -> Bool {
+        switch potentialBracket {
+            case "{", "}", "(", ")", "[", "]":
+                return true
+            default:
+                return false
+        }
+    }
+    
+    func isOpen() -> Bool {
+        switch self.character {
+            case "{", "(", "[":
+                return true
+            default:
+                return false
+        }
+    }
+    
+    func isClosedBy(potentialClose: Bracket) -> Bool {
+        if self.character == "{" && potentialClose.character == "}" {
+            return true
+        }
+        
+        if self.character == "[" && potentialClose.character == "]" {
+            return true
+        }
+        
+        if self.character == "(" && potentialClose.character == ")" {
+            return true
+        }
+        
+        return false
+    }
+}
+
 func hasWellFormedBrackets(string: String) -> Bool {
-    return true
+    
+    var stack = [Bracket]()
+    
+    for char in string.characters {
+        if let bracket = Bracket(potentialBracket: char) {
+            if let lastBracket = stack.last {
+                if lastBracket.isClosedBy(bracket) {
+                    stack.removeLast()
+                    
+                    continue
+                }
+            }
+            stack.append(bracket)
+        }
+    }
+    
+    if stack.isEmpty {
+        return true
+    }
+    
+    return false
 }
 
 //: ## Testing
@@ -47,4 +113,5 @@ hasWellFormedBrackets("{)")
 hasWellFormedBrackets("{]")
 hasWellFormedBrackets("hello {(}")
 hasWellFormedBrackets("((((} goodbye")
+
 
