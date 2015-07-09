97b8115009bf2fe1d0646bd112c3db03ad9171b4
Ryan Grey <rgrey@scottlogic.co.uk>
2015-07-09 09:33:48 +0100

    Adds stub hasWellFormedBrackets function.
    Also adds "tests"

--- StudyGroup.playground/Pages/Brackets.xcplaygroundpage/Contents.swift
+++ StudyGroup.playground/Pages/Brackets.xcplaygroundpage/Contents.swift
@@ -1,7 +1,50 @@
-//: [Previous](@previous)
+//: [Palindromes](@previous)
+
+/*: 
+# Brackets
+
+Test if a `String` has well formed brackets. i.e. `([])` is valid and `(((((}` is invalid.
+*/
+
 
 import Foundation
 
-var str = "Hello, playground"
+//: ## Implementation
 
-//: [Next](@next)
+func hasWellFormedBrackets(string: String) -> Bool {
+    return true
+}
+
+//: ## Testing
+
+//: ### Expect `true`
+hasWellFormedBrackets("")
+hasWellFormedBrackets(" ")
+hasWellFormedBrackets("a")
+hasWellFormedBrackets(" a ")
+hasWellFormedBrackets("()")
+hasWellFormedBrackets("{}")
+hasWellFormedBrackets("()")
+hasWellFormedBrackets("(hello)")
+hasWellFormedBrackets(" (hi)")
+hasWellFormedBrackets("([])")
+hasWellFormedBrackets("([{}])")
+hasWellFormedBrackets("{[()]}")
+hasWellFormedBrackets("  { hello (world [ goodbye { world } . ] ! ) ? } end ")
+
+//: ### Expect `false`
+hasWellFormedBrackets("(")
+hasWellFormedBrackets(")")
+hasWellFormedBrackets("[")
+hasWellFormedBrackets("]")
+hasWellFormedBrackets("{")
+hasWellFormedBrackets("}")
+hasWellFormedBrackets("(]")
+hasWellFormedBrackets("(}")
+hasWellFormedBrackets("[)")
+hasWellFormedBrackets("[}")
+hasWellFormedBrackets("{)")
+hasWellFormedBrackets("{]")
+hasWellFormedBrackets("hello {(}")
+hasWellFormedBrackets("((((} goodbye")
+
